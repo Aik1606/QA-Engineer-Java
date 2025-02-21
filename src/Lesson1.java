2f52e4aa671cebdcd3fec606a89e22c5e34c1036
Ainur Khairullin <aika-88@inbox.ru>
2025-02-21 18:56:12 +0300

    Task2

--- src/Lesson1.java
+++ src/Lesson1.java
@@ -1,7 +1,25 @@
 public class Lesson1 {
+  /**
+   * task1
+   */
   public void printThreeWords() {
     System.out.println("Orange");
     System.out.println("Banana");
     System.out.println("Apple");
+  }
+
+  /**
+   * task2
+   */
+  public void checkSumSign() {
+    int a = 42;
+    int b = 69;
+    int sum = a + b;
+
+    if (sum < 0) {
+      System.out.println("Сумма отрицательная");
+    } else {
+      System.out.println("Сумма положительная");
+    }
   }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,11 @@
 public class Main {
   public static void main(String[] args) {
     Lesson1 lesson1 = new Lesson1();
+
     System.out.println("Task 1 \n");
     lesson1.printThreeWords();
+
+    System.out.println("\nTask 2 \n");
+    lesson1.checkSumSign();
   }
 }
