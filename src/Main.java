6d19cf3400072338609a5f470fa4281a969490eb
Ainur Khairullin <aika-88@inbox.ru>
2025-02-28 23:30:51 +0300

    done

--- src/Main.java
+++ src/Main.java
@@ -2,32 +2,32 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Figure triangle = new Triangle(3.9, 5.0, 7.9, "green", "blue");
-        Figure rectangle = new Rectangle(3.9, 5.0, "orange", "red");
-        Figure circle = new Circle(34,  "black", "pink");
-        System.out.println(circle.getFillColor());
-        System.out.println(circle.getArea());
-        System.out.println(circle.getPerimeter());
-        System.out.println(circle.getBoundaryColor());
-    Cat cat = new Cat("Барсик", 28);
-//  cat.run(120);
-//  cat.run(-3);
-//  cat.run(500);
-//  cat.swim(100);
-//
-//  System.out.printf("Создано %d животных%n", Animal.getCount());
-//
-//  Animal dog = new Dog("Ибрагим");
-//  dog.run(450);
-//  dog.run(550);
-//  dog.run(-1);
-//  dog.swim(5);
-//  dog.swim(10);
-//  dog.swim(-1);
-//
-//    System.out.printf("Создано %d животных%n", Animal.getCount());
+      Figure triangle = new Triangle(3.9, 5.0, 7.9, "green", "blue");
+      Figure rectangle = new Rectangle(3.9, 5.0, "orange", "red");
+      Figure circle = new Circle(34,  "black", "pink");
+      System.out.println(circle.getFillColor());
+      System.out.println(circle.getArea());
+      System.out.println(circle.getPerimeter());
+      System.out.println(circle.getBoundaryColor());
+      Cat cat = new Cat("Барсик", 28);
+      cat.run(120);
+      cat.run(-3);
+      cat.run(500);
+      cat.swim(100);
 
-    catEat(cat);
+      System.out.printf("Создано %d животных%n", Animal.getCount());
+
+      Animal dog = new Dog("Ибрагим");
+      dog.run(450);
+      dog.run(550);
+      dog.run(-1);
+      dog.swim(5);
+      dog.swim(10);
+      dog.swim(-1);
+
+      System.out.printf("Создано %d животных%n", Animal.getCount());
+
+      catEat(cat);
   }
 
   static public void catEat(Cat cat) {
