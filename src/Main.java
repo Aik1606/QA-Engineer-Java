a6a88c18554d609d5cfc1bba5b27bc3bbc65a716
Ainur Khairullin <aika-88@inbox.ru>
2025-03-07 23:37:46 +0300

    Task1

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,22 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    String[][] myArr = {new String[4], new String[4], new String[5], new String[4]};
+    firstExercise(myArr);
+    }
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    private static void firstExercise(String[][] array) {
+        int columnsCount = array.length;
+
+        if (columnsCount != 4) {
+            throw new MyArraySizeException("размер не равен 4x4");
+        }
+
+        for (int i = 0; i < columnsCount; i++) {
+            int rowsCount = array[i].length;
+            if (rowsCount != 4) {
+                throw new MyArraySizeException("размер не равен 4x4");
+            }
         }
     }
 }
