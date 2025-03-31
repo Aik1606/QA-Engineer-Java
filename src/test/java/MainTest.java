240a8efab08766acc2dcbe6d8a7be5c77a92d98e
Ainur Khairullin <aika-88@inbox.ru>
2025-03-31 14:41:29 +0300

    Task 1, 2, 3

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,8 +1,6 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
--- src/test/java/MainTest1.java
+++ src/test/java/MainTest1.java
@@ -1,8 +1,15 @@
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.By;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.time.Instant;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -11,9 +18,9 @@ class MainTest1 {
 
   @BeforeEach
   void setUp() {
+    driver.manage().window().maximize();
     driver.get("https://www.mts.by/");
     driver.findElement(By.id("cookie-agree")).click();
-
   }
 
   @AfterEach
