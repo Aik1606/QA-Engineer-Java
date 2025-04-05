66b686972753c5c5e9185b79ea49d6aaded2a579
Ainur Khairullin <aika-88@inbox.ru>
2025-04-05 17:35:33 +0300

    Task 1, 2

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -4,11 +4,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.time.Duration;
-import java.util.List;
 
 
 class BaseTest {
@@ -20,12 +15,15 @@ class BaseTest {
     driver.get("https://www.mts.by/");
 
     WebElement cookie = driver.findElement(By.id("cookie-agree"));
-    //cookie.click();
+
+    if (cookie.isDisplayed()) {
+      cookie.click();
+    }
 
   }
 
   @AfterEach
   void tearDown() {
-    //driver.quit();
+    driver.quit();
   }
 }
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -2,85 +2,86 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.junit.jupiter.api.Nested;
 
 
-public class MainTest extends BaseTest{
+public class MainTest extends BaseTest {
 
     private static final Logger log = LoggerFactory.getLogger(MainTest.class);
 
     //рассрочка
     @Test
     public void checkPlaceholdersForInstalment() {
-      OnlineReplenishmentPage page = new OnlineReplenishmentPage(driver);
-      page.selectServiceOption("Рассрочка");
+        OnlineReplenishmentPage page = new OnlineReplenishmentPage(driver);
+        page.selectServiceOption("Рассрочка");
 
-      Assertions.assertEquals("E-mail для отправки чека", page.getEmailInstalmentPlaceholder());
-      Assertions.assertEquals("Сумма", page.getSumIInstalmentPlaceholder());
-      Assertions.assertEquals("Номер счета на 44", page.getScoreInstalmentPlaceholder());
+        Assertions.assertEquals("E-mail для отправки чека", page.getEmailInstalmentPlaceholder());
+        Assertions.assertEquals("Сумма", page.getSumIInstalmentPlaceholder());
+        Assertions.assertEquals("Номер счета на 44", page.getScoreInstalmentPlaceholder());
     }
 
-  //услуги связи
+    //услуги связи
 
-  @Test
-  public void checkCommunicationServices() {
-    OnlineReplenishmentPage page = new OnlineReplenishmentPage(driver);
-    page.selectServiceOption("Услуги связи");
+    @Test
+    public void checkCommunicationServices() {
+        OnlineReplenishmentPage page = new OnlineReplenishmentPage(driver);
+        page.selectServiceOption("Услуги связи");
 
-    Assertions.assertEquals("E-mail для отправки чека", page.getConnectionEmailPlaceholder());
-    Assertions.assertEquals("Сумма", page.getConnectionSumPlaceholder());
-    Assertions.assertEquals("Номер телефона", page.getConnectionPhonePlaceholder());
-  }
+        Assertions.assertEquals("E-mail для отправки чека", page.getConnectionEmailPlaceholder());
+        Assertions.assertEquals("Сумма", page.getConnectionSumPlaceholder());
+        Assertions.assertEquals("Номер телефона", page.getConnectionPhonePlaceholder());
+    }
 
-  //дом интернет
-  @Test
-  public void checkHomeInternet() {
-    OnlineReplenishmentPage page = new OnlineReplenishmentPage(driver);
-    page.selectServiceOption("Домашний интернет");
+    //дом интернет
+    @Test
+    public void checkHomeInternet() {
+        OnlineReplenishmentPage page = new OnlineReplenishmentPage(driver);
+        page.selectServiceOption("Домашний интернет");
 
-    Assertions.assertEquals("E-mail для отправки чека", page.getInternetEmailPlaceholder());
-    Assertions.assertEquals("Сумма", page.getInternetSumPlaceholder());
-    Assertions.assertEquals("Номер абонента", page.getInternetPhonePlaceholder());
-  }
+        Assertions.assertEquals("E-mail для отправки чека", page.getInternetEmailPlaceholder());
+        Assertions.assertEquals("Сумма", page.getInternetSumPlaceholder());
+        Assertions.assertEquals("Номер абонента", page.getInternetPhonePlaceholder());
+    }
 
-  //задолженность
+    //задолженность
 
-  @Test
-  public void checkArrears() {
-    OnlineReplenishmentPage page = new OnlineReplenishmentPage(driver);
-    page.selectServiceOption("Задолженность");
+    @Test
+    public void checkArrears() {
+        OnlineReplenishmentPage page = new OnlineReplenishmentPage(driver);
+        page.selectServiceOption("Задолженность");
 
-    Assertions.assertEquals("E-mail для отправки чека", page.getArrearsEmailPlaceholder());
-    Assertions.assertEquals("Сумма", page.getArrearsSumPlaceholder());
-    Assertions.assertEquals("Номер счета на 2073", page.getArrearsScorePlaceholder());
-  }
+        Assertions.assertEquals("E-mail для отправки чека", page.getArrearsEmailPlaceholder());
+        Assertions.assertEquals("Сумма", page.getArrearsSumPlaceholder());
+        Assertions.assertEquals("Номер счета на 2073", page.getArrearsScorePlaceholder());
+    }
 
 
-  //услуги связи заполнение формы
+    //услуги связи заполнение формы
 
-  @Test
-  public void checkCommunicationServices1() {
-    OnlineReplenishmentPage page = new OnlineReplenishmentPage(driver);
-    PaymentConformationPage conformationPage = page
-        .selectServiceOption("Услуги связи")
-        .fillPhoneNumberField("297777777")
-        .fillSumField("10")
-        .submitReplenishmentForm().getPaymentConformationPage();
+    @Test
+    public void checkCommunicationServices1() {
+        String payConst = "123";
+        OnlineReplenishmentPage page = new OnlineReplenishmentPage(driver);
+        PaymentConformationPage conformationPage = page
+                .selectServiceOption("Услуги связи")
+                .fillPhoneNumberField("297777777")
+                .fillSumField(payConst)
+                .submitReplenishmentForm()
+                .getPaymentConformationPage();
 
-   Assertions.assertTrue(conformationPage.getPayDescriptionCost().contains("10"));
-   Assertions.assertTrue(conformationPage.getSubmitButtonText().contains("10"));
-   Assertions.assertTrue(conformationPage.getPayDescriptionText().contains("375297777777"));
-   Assertions.assertNotNull(conformationPage.getVisaIcon());
-   Assertions.assertNotNull(conformationPage.getMaestroIcon());
-   Assertions.assertNotNull(conformationPage.getMaterCardIcon());
-   Assertions.assertNotNull(conformationPage.getBelkartIcon());
-   Assertions.assertNotNull(conformationPage.getMirIcon());
-   Assertions.assertEquals("Номер карты", conformationPage.getCardNumberPlaceholderText());
-   Assertions.assertEquals("Срок действия", conformationPage.getDatePlaceholderText());
-   Assertions.assertEquals("CVC", conformationPage.getCvcPlaceholderText());
-   Assertions.assertEquals("Имя держателя (как на карте)", conformationPage.getHolderNamePlaceholderText());
+        Assertions.assertTrue(conformationPage.getPayDescriptionCost().contains(payConst));
+        Assertions.assertTrue(conformationPage.getSubmitButtonText().contains(payConst));
+        Assertions.assertTrue(conformationPage.getPayDescriptionText().contains("375297777777"));
+        Assertions.assertNotNull(conformationPage.getVisaIcon());
+        Assertions.assertNotNull(conformationPage.getMaestroIcon());
+        Assertions.assertNotNull(conformationPage.getMaterCardIcon());
+        Assertions.assertNotNull(conformationPage.getBelkartIcon());
+        Assertions.assertNotNull(conformationPage.getMirIcon());
+        Assertions.assertEquals("Номер карты", conformationPage.getCardNumberPlaceholderText());
+        Assertions.assertEquals("Срок действия", conformationPage.getDatePlaceholderText());
+        Assertions.assertEquals("CVC", conformationPage.getCvcPlaceholderText());
+        Assertions.assertEquals("Имя держателя (как на карте)", conformationPage.getHolderNamePlaceholderText());
 
-  }
+    }
 
 }
 
